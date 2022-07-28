ea8e43d4876325bb3876417ddeac1e23e000fc04
thiswhitenike@gmail.com <thiswhitenike@gmail.com>
2022-07-28 09:21:51 +0700

    Создано решение

---
+++ CPWDLL/CPWDLL/MSLDLL/CPW.f90
@@ -0,0 +1,8 @@
+    subroutine CPW(t, h, e, aw, s, dC, dL, em, dZ0)
+    !dec$ attributes dllexport ::CPW
+    !DEC$ ATTRIBUTES VALUE :: t, h, e, aw
+    !DEC$ ATTRIBUTES REFERENCE :: s, dC, dL, em, dZ0
+    implicit real*8(a-y)
+    dimension s(2)
+    call main(t, h, e, aw, s, dC, dL, em, dZ0)
+    end
